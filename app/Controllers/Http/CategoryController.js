1eb96c23f9762db9a05cbea047d3557f67f554be
Carlos Ferreira <conta@especializati.com.br>
2019-05-06 11:03:44 -0300

    List categories api

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -1,6 +1,15 @@
 'use strict'
 
+const Category = use('App/Models/Category')
+
 class CategoryController {
+
+    async index () {
+        const categories = await Category.all()
+
+        return categories
+    }
+
 }
 
 module.exports = CategoryController
